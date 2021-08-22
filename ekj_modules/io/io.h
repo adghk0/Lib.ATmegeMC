d5d09f53e03cfd255994272c6e830904ba3d13ef
Eom Kyoungjun <adghk01234@gmail.com>
2021-08-22 23:16:55 +0900

    simple io library updated

---
+++ ekj_modules/io/io.h
@@ -0,0 +1,36 @@
+﻿/*
+ * io.h
+ * easy normal port controls
+ * Created: 2021-08-22 오후 10:47:11
+ *  Author: ekj
+ */ 
+
+
+#ifndef IO_H_
+#define IO_H_
+
+#define IO_INPUT	0
+#define IO_OUTPUT	1
+
+void io_dir(volatile uint8_t *ddr, unsigned char bit, unsigned char dir)
+{
+	*ddr = (*ddr&~(1<<bit)) | (dir*(1<<bit));
+}
+void io_set(volatile uint8_t *port, unsigned char bit)
+{
+	*port |= (1<<bit);
+}
+void io_clear(volatile uint8_t *port, unsigned char bit)
+{
+	*port &= ~(1<<bit);
+}
+void io_write(volatile uint8_t *port, unsigned char bit, unsigned char val)
+{
+	*port = (*port&~(1<<bit)) | (val*(1<<bit));
+}
+char io_read(volatile uint8_t *pin, unsigned char bit)
+{
+	return (*pin & (1<<bit)) != 0;
+}
+
+#endif /* IO_H_ */
---
+++ ekj_modules/sensor/dht11/dht11.h
@@ -0,0 +1,16 @@
+﻿/*
+ * dht11.h
+ *
+ * Created: 2021-08-22 오후 11:16:26
+ *  Author: ekj
+ */ 
+
+
+#ifndef DHT11_H_
+#define DHT11_H_
+
+
+
+
+
+#endif /* DHT11_H_ */
