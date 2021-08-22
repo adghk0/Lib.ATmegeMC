9d2dcd13a722aa0ca668b4819b192006160dc908
Eom Kyoungjun <adghk01234@gmail.com>
2021-08-22 17:59:02 +0900

    Analog to Digital Converter library updated

---
+++ ekj_modules/adc/adc.c
@@ -0,0 +1,53 @@
+﻿/*
+ * adc.c
+ *
+ * Created: 2021-08-22 오후 5:17:39
+ *  Author: ekj
+ */ 
+
+#include "adc.h"
+
+unsigned int buf = 0;
+unsigned char finish = 0;
+
+ISR(ADC_vect)
+{
+	buf = ADCW;
+	finish = 1;
+}
+
+// initializing ADC
+void adc_init(unsigned long f_cpu, unsigned char adlar, unsigned char ref)
+{
+	// ADC Enable, ADC Interrupt Enable, ADC Prescaler 128
+	ADCSRA = (1<<ADEN) | (1<<ADIE) | (7<<ADPS0);
+	// set reference voltage and left adjust result bit
+	ADMUX = (ref<<REFS0) | (adlar<<ADLAR);
+}
+
+// request starting conversion
+void adc_request(unsigned char channel)
+{
+	finish = 0;
+	
+	// set channel
+	ADMUX = (ADMUX & 0xf0) | channel;
+	
+	// start conversion
+	ADCSRA |= (1<<ADSC);
+}
+
+// can read the data
+// Returns : exist(1), no-exist(0)
+char adc_available()
+{
+	return finish;
+}
+
+// read data
+// Returns : data(16bit)
+int adc_read()
+{
+	finish = 1;
+	return buf;
+}
---
+++ ekj_modules/adc/adc.h
@@ -0,0 +1,44 @@
+﻿/*
+ * adc.h
+ *
+ * Created: 2021-08-22 오후 4:55:42
+ *  Author: ekj
+ */ 
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#define ADC_CHANNEL0	0
+#define ADC_CHANNEL1	1
+#define ADC_CHANNEL2	2
+#define ADC_CHANNEL3	3
+#define ADC_CHANNEL4	4
+#define ADC_CHANNEL5	5
+#define ADC_CHANNEL6	6
+#define ADC_CHANNEL7	7
+#define ADC_CHANNEL8	8
+
+#define ADC_ALIGN_LSB	0
+#define ADC_ALIGN_MSB	1
+
+#define ADC_REF_AREF	0
+#define ADC_REF_AVCC	1
+#define ADC_REF_INTER	3
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/common.h>
+
+// initializing ADC
+void adc_init(unsigned long f_cpu, unsigned char adlar, unsigned char ref);
+// request starting conversion
+void adc_request(unsigned char channel);
+// can read the data
+// Returns : exist(1), no-exist(0)
+char adc_available();
+// read data
+// Returns : data(16bit)
+int adc_read();
+
+
+#endif /* ADC_H_ */
