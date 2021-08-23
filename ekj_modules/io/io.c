cb4a019cef31c74d2a579082f4633bfddcbb04ba
Eom Kyoungjun <adghk01234@gmail.com>
2021-08-23 19:12:13 +0900

    change code io.h with typedef structure and find error in uart.h

--- ekj_modules/io/io.c
+++ ekj_modules/io/io.c
@@ -1,40 +1,85 @@
 ﻿/*
  * io.c
- *
- * Created: 2021-08-22 오후 11:21:54
- *  Author: ekj
+ * the basic one pin controls
+ * Created: 2021-08-23 오후 6:31:20
+ *  Author: Eom Kyoungjun
  */ 
 
 #include "io.h"
 
-void io_dir(volatile uint8_t *ddr, unsigned char bit, unsigned char dir)
+/*** FUNCTIONS ***/
+
+// make a new pin structure
+io_pin* io_new(volatile uint8_t *port, unsigned char bit)
 {
-	*ddr = (*ddr&~(1<<bit)) | (dir*(1<<bit));
-}
-void io_set(volatile uint8_t *port, unsigned char bit)
-{
-	*port |= (1<<bit);
-}
-void io_clear(volatile uint8_t *port, unsigned char bit)
-{
-	*port &= ~(1<<bit);
-}
-void io_write(volatile uint8_t *port, unsigned char bit, unsigned char val)
-{
-	*port = (*port&~(1<<bit)) | (val*(1<<bit));
-}
-char io_read(volatile uint8_t *pin, unsigned char bit)
-{
-	return (*pin >> bit) & 1;
-}
-int io_wait(volatile uint8_t *pin, unsigned char bit, unsigned char obj, int timeout)
-{
-	int time_us = 0;
+	io_pin *pin;
+	pin = malloc(sizeof(io_pin));
 	
-	while(time_us++ < timeout && io_read(pin, bit) != obj) _delay_us(1);
+	pin->port = port;
+	pin->ddr = port - 1;
+	pin->pin = port - 2;
+	pin->bit = bit;
 	
-	if(time_us < timeout)
-		return time_us;
+	return pin;
+}
+
+// set direction
+void io_dir(volatile io_pin *pin, char direction)
+{
+	*pin->ddr = (*pin->ddr&~(1<<pin->bit)) | (direction*(1<<pin->bit));
+}
+
+// set the pin (1)
+void io_set(volatile io_pin *pin)
+{
+	*pin->port |= (1<<pin->bit);
+}
+
+// clear the pin (0)
+void io_clear(volatile io_pin *pin)
+{
+	*pin->port &= ~(1<<pin->bit);	
+}
+
+// write the value to pin
+void io_write(volatile io_pin *pin, char value)
+{
+	*pin->port = (*pin->port&~(1<<pin->bit)) | (value*(1<<pin->bit));
+}
+
+// get the output value at pin
+char io_get(volatile io_pin *pin)
+{
+	return (*pin->port >> pin->bit) & 1;
+}
+
+// read the value from pin
+char io_read(volatile io_pin *pin)
+{
+	return (*pin->pin >> pin->bit) & 1;
+}
+// wait until pin be target in timeout[us]
+int io_wait_us(volatile io_pin *pin, char target, int timeout)
+{
+	int t = 0;
+	
+	while(t++ < timeout && io_read(pin) != target) _delay_us(1);
+	
+	if(t < timeout)
+		return t;
 	else
-		return -1; 
+		return -1;
+}
+
+// wait until pin be target in timeout[ms]
+int io_wait_ms(volatile io_pin *pin, char target, int timeout)
+{
+	int t = 0;
+	
+	while(t++ < timeout && io_read(pin) != target) _delay_ms(1);
+	
+	if(t < timeout)
+	return t;
+	else
+	return -1;
 }
--- ekj_modules/io/io.h
+++ ekj_modules/io/io.h
@@ -1,27 +1,57 @@
 ﻿/*
  * io.h
- * easy normal port controls
- * Created: 2021-08-22 오후 10:47:11
- *  Author: ekj
+ * the basic one pin controls
+ * Created: 2021-08-23 오후 5:18:01
+ *  Author: Eom Kyoungjun
  */ 
 
+/*** DEFINES ***/
 
 #ifndef IO_H_
 #define IO_H_
 
-#define F_CPU 16000000UL
-
 #define IO_INPUT	0
 #define IO_OUTPUT	1
 
+#define IO_FALSE	0
+#define IO_TRUE		1
+
+/*** INCLUDES ***/
+#include <stdlib.h>
 #include <stdint.h>
 #include <util/delay.h>
 
-void io_dir(volatile uint8_t *ddr, unsigned char bit, unsigned char dir);
-void io_set(volatile uint8_t *port, unsigned char bit);
-void io_clear(volatile uint8_t *port, unsigned char bit);
-void io_write(volatile uint8_t *port, unsigned char bit, unsigned char val);
-char io_read(volatile uint8_t *pin, unsigned char bit);
-int io_wait(volatile uint8_t *pin, unsigned char bit, unsigned char obj, int timeout);
+
+/*** TYPES ***/
+
+// the type describing a pin
+typedef struct io_pin
+{
+	volatile uint8_t *ddr;
+	volatile uint8_t *port;
+	volatile uint8_t *pin;
+	unsigned char bit;
+} io_pin;
+
+/*** FUNCTIONS ***/
+
+// make a new pin structure
+io_pin* io_new(volatile uint8_t *port, unsigned char bit);
+// set direction
+void io_dir(volatile io_pin *pin, char direction);
+// set the pin (1)
+void io_set(volatile io_pin *pin);
+// clear the pin (0)
+void io_clear(volatile io_pin *pin);
+// write the value to pin
+void io_write(volatile io_pin *pin, char value);
+// get the output value at pin
+char io_get(volatile io_pin *pin);
+// read the value from pin
+char io_read(volatile io_pin *pin);
+// wait until pin be target in timeout[us]
+int io_wait_us(volatile io_pin *pin, char target, int timeout);
+// wait until pin be target in timeout[ms]
+int io_wait_ms(volatile io_pin *pin, char target, int timeout);
 
 #endif /* IO_H_ */
